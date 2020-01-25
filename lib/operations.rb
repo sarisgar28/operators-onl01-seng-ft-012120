7c656f78c04a8498cc303214e08bf264f9969de5
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-25 18:47:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
-def unsafe?(speed)
-
+def  age; == "24"
+  puts sara_is_24
+  if "she says so"
+  print why_does_she_act_like_30
+  (?:) ==< "24"
+end
 end
 
 
 
-def not_safe?(speed)
-	
+def if the speed is =< ("30 mph")
+	(?:) would_be_unsafe 
+	else as_going_95mph
+	  print "going 50 miles per hour, however would"
+	  return "!true"
+	  #//!true 
 end
 	
 
