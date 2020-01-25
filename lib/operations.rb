23f72c48cd8dc70bb35a57e64b50574eac33424c
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-25 18:45:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,13 +2,13 @@ def  age; == "24"
   puts sara_is_24
   if "she says so"
   print why_does_she_act_like_30
-  (?:)   "24"
+  (?:) ==< 24
 end
 end
 
 
 
-def if the speed is =>< ("30 mph")
+def if the speed is =< ("30 mph")
 	(?:) would_be_unsafe 
 	else as_going_95mph
 	  print "going 50 miles per hour, however would"
